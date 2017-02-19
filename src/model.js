2d8fe4e1d5c5779e485018eff1eaddb4f006a1b4
M.C. van den Boogaart <scola@kvdb.name>
2017-02-19 19:36:42 +0100

    Replace stream with subscribe, refactor

--- src/model.js
+++ src/model.js
@@ -20,33 +20,61 @@ export default class Model extends EventEmitter {
 
     this._parser = null;
     this._keys = [];
-    this._state = 'idle';
 
     this._serialize = (o) => o;
     this._deserialize = (o) => o;
 
     this._local = {};
     this._remote = null;
+    this._total = null;
+    this._etag = null;
 
     this._request = null;
     this._response = null;
-    this._stream = false;
 
-    this._handleSocket = () => this._socket();
+    this._auth = null;
+    this._subscribe = false;
+
+    this._handleOpen = () => this._open();
+
+    this._handleData = (d) => this._data(d);
+    this._handleEnd = () => this._end();
+    this._handleError = (e) => this._error(e);
+
+    this._handleSelect = (r) => this._select(r);
+    this._handleInsert = (r) => this._insert(r);
+    this._handleUpdate = (r) => this._update(r);
+    this._handleDelete = (r) => this._delete(r);
+
     this._objectMode();
   }
 
   destroy() {
-    if (this._request) {
-      this._request.end();
-    }
-
+    this._end();
     this._unbindConnection();
 
     this._connection = null;
     this._cache = null;
     this._serialize = null;
     this._unserialize = null;
+  }
+
+  auth(value = null) {
+    if (value === null) {
+      return this._auth;
+    }
+
+    this._auth = value;
+    return this;
+  }
+
+  cache(value = null) {
+    if (value === null) {
+      return this._cache;
+    }
+
+    this._cache = value;
+    return this;
   }
 
   connection(value = null) {
@@ -60,12 +88,17 @@ export default class Model extends EventEmitter {
     return this;
   }
 
-  cache(value = null) {
+  mode(value = null) {
     if (value === null) {
-      return this._cache;
+      return this._mode;
     }
 
-    this._cache = value;
+    if (value === 'list') {
+      this._listMode();
+    } else if (value === 'object') {
+      this._objectMode();
+    }
+
     return this;
   }
 
@@ -82,15 +115,19 @@ export default class Model extends EventEmitter {
     return this;
   }
 
-  mode(value = null) {
-    if (value === null) {
-      return this._mode;
+  subscribe(action = null) {
+    if (action === null) {
+      return this._subscribe;
     }
 
-    if (value === 'list') {
-      this._listMode();
-    } else if (value === 'object') {
-      this._objectMode();
+    this._subscribe = action;
+
+    if (action === false) {
+      if (this._request) {
+        this._request.end();
+      }
+
+      this._end();
     }
 
     return this;
@@ -135,6 +172,10 @@ export default class Model extends EventEmitter {
     }
 
     return this;
+  }
+
+  total() {
+    return this._total;
   }
 
   get(name) {
@@ -225,179 +266,155 @@ export default class Model extends EventEmitter {
     this._cache.delete(this._key(), callback);
   }
 
-  stream(action = null) {
-    if (action === null) {
-      return this._stream;
-    }
-
-    this._stream = action;
-
-    if (action === false) {
-      this._request.end();
-      this._request = null;
-
-      return this;
-    }
-
-    this.select();
-    return this;
-  }
-
   select() {
-    if (this._state === 'busy') {
+    if (this._subscribe === true) {
+      this._end();
+    }
+
+    if (this._request) {
       return this;
     }
 
-    this._state = 'busy';
     const [path, local = {}] = this._parse();
 
-    const request = this._connection
+    this._request = this._connection
       .request()
       .method('GET')
       .path(path);
 
     if (this._mode === 'list') {
-      request.query(local);
+      this._request.query(local);
     }
 
-    request.once('error', (error) => {
-      this._state = 'idle';
-      this.emit('error', error);
-    });
+    if (this._etag) {
+      this._request.header('x-etag', this._etag);
+    }
 
-    request.once('response', (response) => {
-      this._handleSelect(response);
-    });
+    this._request.once('error', this._handleError);
+    this._request.once('response', this._handleSelect);
 
-    if (this._stream === true) {
-      this._request = request;
+    if (this._subscribe === true) {
       this._request.write('');
     } else {
-      request.end();
+      this._request.end();
     }
 
     return this;
   }
 
   insert() {
-    if (this._state === 'busy') {
+    if (this._request) {
       return this;
     }
 
-    this._state = 'busy';
     const [path, local] = this._parse();
 
-    const request = this._connection
+    this._request = this._connection
       .request()
       .method('POST')
       .path(path);
 
-    request.once('error', (error) => {
-      this._state = 'idle';
-      this.emit('error', error);
-    });
+    this._request.once('error', this._handleError);
+    this._request.once('response', this._handleInsert);
+    this._request.end(local);
 
-    request.once('response', (response) => {
-      this._handleInsert(response);
-    });
-
-    request.end(local);
     return this;
   }
 
   update() {
-    if (this._state === 'busy') {
+    if (this._request) {
       return this;
     }
 
-    this._state = 'busy';
     const [path, local] = this._parse();
 
-    const request = this._connection
+    this._request = this._connection
       .request()
       .method('PUT')
       .path(path);
 
-    request.once('error', (error) => {
-      this._state = 'idle';
-      this.emit('error', error);
-    });
+    this._request.once('error', this._handleError);
+    this._request.once('response', this._handleUpdate);
+    this._request.end(local);
 
-    request.once('response', (response) => {
-      this._handleUpdate(response);
-    });
-
-    request.end(local);
     return this;
   }
 
   delete() {
-    if (this._state === 'busy') {
+    if (this._request) {
       return this;
     }
 
-    this._state = 'busy';
     const [path] = this._parse();
 
-    const request = this._connection
+    this._request = this._connection
       .request()
       .method('DELETE')
       .path(path);
 
-    request.once('error', (error) => {
-      this._state = 'idle';
-      this.emit('error', error);
-    });
+    this._request.once('error', this._handleError);
+    this._request.once('response', this._handleDelete);
+    this._request.end();
 
-    request.once('response', (response) => {
-      this._handleDelete(response);
-    });
-
-    request.end();
     return this;
   }
 
   _bindConnection() {
-    this._connection.on('socket', this._handleSocket);
-  }
-
-  _unbindConnection() {
-    this._connection.removeListener('socket', this._handleSocket);
-  }
-
-  _handleSelect(response) {
-    this._state = 'idle';
-
-    response.once('error', (error) => {
-      this.emit('error', error);
-    });
-
-    response.on('data', (data) => {
-      if (response.status() >= 400) {
-        this.emit('error', new ScolaError(data));
-        return;
+    if (this._connection) {
+      if (this._auth === true) {
+        this._connection.on('user', this._handleOpen);
+      } else {
+        this._connection.on('open', this._handleOpen);
       }
-
-      this.remote(data);
-      this.emit('select', data);
-    });
-
-    response.once('end', () => {
-      if (this._request) {
-        this._request.destroy();
-        this._request = null;
-      }
-
-      this._response = null;
-    });
-
-    if (this._stream) {
-      this._response = response;
     }
   }
 
-  _handleInsert(response) {
-    this._state = 'idle';
+  _unbindConnection() {
+    if (this._connection) {
+      if (this._auth === true) {
+        this._connection.removeListener('user', this._handleOpen);
+      } else {
+        this._connection.removeListener('open', this._handleOpen);
+      }
+    }
+  }
+
+  _select(response) {
+    this._response = response;
+    this._response.once('error', this._handleError);
+    this._response.once('end', this._handleEnd);
+    this._response.on('data', this._handleData);
+  }
+
+  _data(data) {
+    if (this._response.status() >= 400) {
+      this._error(new ScolaError(data));
+      return;
+    }
+
+    if (this._response.status() === 304) {
+      return;
+    }
+
+    if (this._response.header('x-change')) {
+      this.emit('change', data);
+      return;
+    }
+
+    if (this._response.header('x-etag')) {
+      this._etag = this._response.header('x-etag');
+    }
+
+    if (this._response.header('x-total')) {
+      this._total = this._response.header('x-total');
+    }
+
+    this.remote(data);
+    this.emit('select', data);
+  }
+
+  _insert(response) {
+    this._end();
 
     extract(response, (error) => {
       if (error) {
@@ -414,8 +431,8 @@ export default class Model extends EventEmitter {
     });
   }
 
-  _handleUpdate(response) {
-    this._state = 'idle';
+  _update(response) {
+    this._end();
 
     extract(response, (error) => {
       if (error) {
@@ -433,8 +450,8 @@ export default class Model extends EventEmitter {
     });
   }
 
-  _handleDelete(response) {
-    this._state = 'idle';
+  _delete(response) {
+    this._end();
 
     extract(response, (error) => {
       if (error) {
@@ -452,6 +469,25 @@ export default class Model extends EventEmitter {
         this.emit('delete');
       });
     });
+  }
+
+  _end() {
+    if (this._request) {
+      this._request.removeAllListeners();
+      this._request.destroy();
+      this._request = null;
+    }
+
+    if (this._response) {
+      this._response.removeAllListeners();
+      this._response.destroy();
+      this._response = null;
+    }
+  }
+
+  _error(error) {
+    this._end();
+    this.emit('error', error);
   }
 
   _listMode() {
@@ -480,8 +516,8 @@ export default class Model extends EventEmitter {
     return [path, local];
   }
 
-  _socket() {
-    if (this._stream === true) {
+  _open() {
+    if (this._subscribe === true) {
       this.select();
     }
   }
