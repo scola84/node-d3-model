a23632e49686382829bdd54b6293670e7af3e5b4
M.C. van den Boogaart <scola@kvdb.name>
2017-04-06 11:28:20 +0200

    Provide checks through chained methods

--- src/state.js
+++ src/state.js
@@ -2,12 +2,13 @@ export default class State {
   constructor() {
     this._conditions = new Map();
     this._values = {};
+    this._last = null;
     this._then = null;
     this._else = null;
   }
 
-  if (name, check) {
-    this._conditions.set(name, check);
+  if (name) {
+    this._last = name;
     return this;
   }
 
@@ -24,6 +25,67 @@ export default class State {
   set(name, value) {
     this._values[name] = value;
     this._check();
+
+    return this;
+  }
+
+  custom(value) {
+    this._conditions.set(this._last, value);
+    return this;
+  }
+
+  defined() {
+    this._conditions.set(this._last, (value) => {
+      return typeof value !== 'undefined';
+    });
+
+    return this;
+  }
+
+  undefined() {
+    this._conditions.set(this._last, (value, set) => {
+      return typeof value === 'undefined' && set === true;
+    });
+
+    return this;
+  }
+
+  false() {
+    this._conditions.set(this._last, (value) => {
+      return value === false;
+    });
+
+    return this;
+  }
+
+  falsy() {
+    this._conditions.set(this._last, (value) => {
+      return Boolean(value) === false;
+    });
+
+    return this;
+  }
+
+  number() {
+    this._conditions.set(this._last, (value) => {
+      return typeof value === 'number';
+    });
+
+    return this;
+  }
+
+  true() {
+    this._conditions.set(this._last, (value) => {
+      return value === true;
+    });
+
+    return this;
+  }
+
+  truthy() {
+    this._conditions.set(this._last, (value) => {
+      return Boolean(value) === true;
+    });
 
     return this;
   }
